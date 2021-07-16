6e9438be05b22c508c8ce4ef715ba6f0bd15afae
Aaron Kauffman <aaron@Aarons-MBP.spanishhead.com>
2021-07-15 18:13:41 -0600

    use predefined constructor to push new object to global array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-$(document).ready(function() {
-console.log("Jquery loaded properly");
-});
+// $(document).ready(function() {
+// console.log("Jquery loaded properly");
+// });
+
+//create empty global array to store objects
+let orders = []
+
+function Pizza(name, cheese, pepperoni, artichoke, anchovy, pineapple){
+    this.cheese = cheese;
+    this. pepperoni = pepperoni;
+    this.artichoke = artichoke;
+    this.anchovy = anchovy;
+    this.pineapple = pineapple;
+}
+
+function orderNewPizza(name, cheese, pepperoni, artichoke, anchovy, pineapple){
+    orders.push(new Pizza(name, cheese, pepperoni, artichoke, anchovy, pineapple));
+    console.log("order list: ")
+    return orders;
+}
